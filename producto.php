8b52216c2ce789be036cb54059f3579d1fe6b2ad
Ivan Yang <yangivan19@gmail.com>
2024-06-08 16:02:24 -0300

    Inicio de la creacion inventario trabajo. Prototipo 1

---
+++ producto.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'includes/consultas_sql.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Producto</title>
+</head>
+<body>
+    <?=require_once 'includes/header.php'?>
+
+    <div class="container">
+
+
+    </div>
+</body>
+</html>
