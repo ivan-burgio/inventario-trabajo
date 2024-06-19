fa240d106383272602a1edf7cd7eeda86e126add
Ivan Yang <yangivan19@gmail.com>
2024-06-19 16:03:30 -0300

    ...

--- pages/comentarios.php
+++ pages/comentarios.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION['user'])) {
         <div class="user">
             <h1 class="name">Bienvenido, <?=$_SESSION['user']['nombre'];?> <?=$_SESSION['user']['apellido'];?></h1>
             <a href="../includes/cerrar_login.php"><img src="../assets/close.svg" alt="Cerrar sesión" /></a>
-        </div>
+        </div>l
         <ul class="list">
             <li><a href="inventario.php">Inventario</a></li>
             <li><a href="registro.php">Registro</a></li>
