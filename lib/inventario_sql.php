06f50c4990b9fe5ba81d9f93d585c63d7afcaf11
Ivan Yang <yangivan19@gmail.com>
2024-06-19 16:01:30 -0300

    Se modifica la consulta sql para traer los productos con status = 1

--- lib/inventario_sql.php
+++ lib/inventario_sql.php
@@ -4,7 +4,7 @@ require_once '../includes/conexion.php';
 
 
 //Se genera la consulta para la BD
-$sql = "SELECT id, marca, modelo, COUNT(modelo) AS stock FROM productos GROUP BY modelo;";
+$sql = "SELECT id, marca, modelo, COUNT(modelo) AS stock FROM productos WHERE status = 1 GROUP BY modelo;";
 
 //Se inserta la consulta en la BD
 $select = mysqli_query($conexion, $sql);
