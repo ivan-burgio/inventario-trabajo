f518859ab10fbca1285bda3d9378ef866742fb1b
Ivan Yang <yangivan19@gmail.com>
2024-06-19 15:59:54 -0300

    Se empieza con la validación para formulario de tele trabajo

--- lib/altas_sql.php
+++ lib/altas_sql.php
@@ -2,10 +2,44 @@
 
 require_once '../includes/conexion.php';
 
+//--------------CONSULTAS MYSQL---------------------
 
 $list = "SELECT f.id_funcionario, f.nombre, f.apellido, s.nombre AS nombre_sector FROM funcionarios f
          INNER JOIN sectores s ON f.sector = s.id_sector";
 $list_query = mysqli_query($conexion, $list);
+
+
+$list_prod = "SELECT id, marca, modelo FROM productos;";
+$list_prod_query = mysqli_query($conexion, $list_prod);
+
+//------------VALIDACIÓN DE CAMPOS-------------------
+
+
+if($_POST['ubic'] == 'Tele trabajo') {
+
+    $equipo = isset($_POST['select_prod']) ? $_POST['select_prod'] : false;
+    $funcionario = isset($_POST['selec_func']) ? $_POST['select_func'] : false;
+    $domicilio = isset($_POST['direc']) ? $_POST['direc'] : false;
+    $descripcion = isset($_POST['description']) ? $_POST['description'] : false;
+
+    $errores = array();
+
+    if(empty($domicilio)) {
+
+        $errores['domicilio'] = "Ingrese el domicilio del funcionario";
+    };
+
+    if(empty($descripcion)) {
+
+        $errores['descripcion'] = "Ingrese una descripción mayor a 20 caracteres";
+    };
+
+    $user = $_SESSION['user']['nombre'];
+
+    $nombre_func_query = "SELECT nombre FROM funcionarios WHERE id_funcionario = '$funcionario';";
+
+    $insert_alta_tt = "INSERT INTO altas_productos VALUES()";
+};
 
 
 ?>
