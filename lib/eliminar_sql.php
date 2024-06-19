26462b76b7afa654bafb9af7be0441a3a30864b8
Ivan Yang <yangivan19@gmail.com>
2024-06-19 16:00:44 -0300

    Se modifica la forma de eliminar de fisica a logica

--- lib/eliminar_sql.php
+++ lib/eliminar_sql.php
@@ -5,12 +5,12 @@ require_once '../includes/conexion.php';
 //Se valida la existencia de GET
 if(isset($_GET)) {
 
-    //Eliminar un elemento de la lista a través de su ID
+    //Se modifica el status de un producto para que no se visualice más
 
-    $id_get = isset($_GET['id']) ? $_GET['id'] : false;             //Se valida la existencia de datos enviados por GET
+    $id_get = isset($_GET['id']) ? $_GET['id'] : false;       //Se valida la existencia de datos enviados por GET
 
     //Se genera la consulta para la BD
-    $delete_pro = "DELETE FROM productos WHERE id = '$id_get';";
+    $delete_pro = "UPDATE productos SET status = 0 WHERE id = '$id_get';";
     
     //Se inserta la consulta en la BD
     $delete_query = mysqli_query($conexion, $delete_pro);
@@ -18,6 +18,5 @@ if(isset($_GET)) {
 
 //Se redirecciona hacia la página de inventario
 header('Location: ../pages/inventario.php');
-exit();
 
 ?>
